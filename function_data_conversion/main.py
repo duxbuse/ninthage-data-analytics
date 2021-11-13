992f8ecb1683a899f4dec54d0cec35d26583aca3
sean-laptop <duxbuse@gmail.com>
2021-11-14 00:25:03 +1100

    trying to save and load from /tmp

--- function_data_conversion/main.py
+++ function_data_conversion/main.py
@@ -37,11 +37,11 @@ def function_data_conversion(request):
     file_name = data["name"]
 
     downloaded_docx_blob = download_blob(bucket_name, file_name)
-    downloaded_docx_blob.download_to_filename(file_name)
+    downloaded_docx_blob.download_to_filename(f"/tmp/{file_name}")
     print(f"Downloaded {file_name} from {bucket_name}")
 
-    converted_file = Convert(file_name)
-    print(f"Converted {file_name} to .json")
+    converted_file = Convert(f"/tmp/{file_name}")
+    print(f"Converted /tmp/{file_name} to .json")
 
     converted_filename = Path(file_name).stem + ".json"
 
