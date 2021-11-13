be48118a65d0a3200c9eda5aa12d16701501cfd5
sean-laptop <duxbuse@gmail.com>
2021-11-14 00:56:31 +1100

    prevent dodgy loops

--- function_data_conversion/main.py
+++ function_data_conversion/main.py
@@ -1,7 +1,5 @@
 from google.cloud import storage
 from pathlib import Path
-
-import requests
 from converter import Convert
 
 
@@ -36,17 +34,20 @@ def function_data_conversion(request):
     bucket_name = data["bucket"]
     file_name = data["name"]
 
-    downloaded_docx_blob = download_blob(bucket_name, file_name)
-    downloaded_docx_blob.download_to_filename(f"/tmp/{file_name}")
-    print(f"Downloaded {file_name} from {bucket_name}")
+    #only convert .docx files, because the json verions are also put back into the bucket there is another trigger
+    if Path(file_name).suffix == ".docx" :
 
-    converted_file = Convert(f"/tmp/{file_name}")
-    print(f"Converted /tmp/{file_name} to .json")
+        downloaded_docx_blob = download_blob(bucket_name, file_name)
+        downloaded_docx_blob.download_to_filename(f"/tmp/{file_name}")
+        print(f"Downloaded {file_name} from {bucket_name}")
 
-    converted_filename = Path(file_name).stem + ".json"
+        converted_file = Convert(f"/tmp/{file_name}")
+        print(f"Converted /tmp/{file_name} to .json")
 
-    upload_blob(bucket_name, converted_file, converted_filename)
-    print(f"Uploaded {converted_filename} to {bucket_name}")
+        converted_filename = Path(file_name).stem + ".json"
+
+        upload_blob(bucket_name, converted_file, converted_filename)
+        print(f"Uploaded {converted_filename} to {bucket_name}")
 
 
 
