6f4f61353dc3e524247b678518c7837928c3d9de
Sean <duxbuse@gmail.com>
2021-11-15 01:20:14 +1100

    ignoring false double call due to laoding json file into same bucket

--- function_upload_data_into_bigquery/main.py
+++ function_upload_data_into_bigquery/main.py
@@ -13,34 +13,36 @@ def download_blob(bucket_name, blob_name):
 
 def function_upload_data_into_bigquery(request):
 
-    print(request.get_json())
+    # print(request.get_json())
 
     request_body = request.get_json()["json_file"]["body"]
     print(request_body)
 
-    filename = request_body["filename"]
-    bucket_name = request_body["bucket_name"]
-    client = bigquery.Client()
-    dataset_id = 'all_lists'
-    table_id = 'tournament_lists'
+    if request_body != "Uploaded file was not of extension \'.docx\' so is being ignored.":
 
-    downloaded_docx_blob = download_blob(bucket_name, filename)
-    downloaded_docx_blob.download_to_filename(f"/tmp/{filename}")
+        filename = request_body["filename"]
+        bucket_name = request_body["bucket_name"]
+        client = bigquery.Client()
+        dataset_id = 'all_lists'
+        table_id = 'tournament_lists'
 
-    dataset_ref = client.dataset(dataset_id)
-    table_ref = dataset_ref.table(table_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
+        downloaded_docx_blob = download_blob(bucket_name, filename)
+        downloaded_docx_blob.download_to_filename(f"/tmp/{filename}")
 
-    with open(f"/tmp/{filename}", "rb") as source_file:
-        job = client.load_table_from_file(
-            source_file,
-            table_ref,
-            location="US",  # Must match the destination dataset location.
-            job_config=job_config,
-        )  # API request
+        dataset_ref = client.dataset(dataset_id)
+        table_ref = dataset_ref.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job_config.autodetect = True
 
-    job.result()  # Waits for table load to complete.
+        with open(f"/tmp/{filename}", "rb") as source_file:
+            job = client.load_table_from_file(
+                source_file,
+                table_ref,
+                location="US",  # Must match the destination dataset location.
+                job_config=job_config,
+            )  # API request
 
-    print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+        job.result()  # Waits for table load to complete.
+
+        print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
