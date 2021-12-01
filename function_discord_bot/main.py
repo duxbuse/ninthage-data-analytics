d52c40a61a38a0e2a021aef3a43c55a980c391b1
Sean <duxbuse@gmail.com>
2021-12-01 13:56:49 +1100

    tryign to upload to the bucket

--- function_discord_bot/main.py
+++ function_discord_bot/main.py
@@ -1,7 +1,9 @@
 from flask.wrappers import Request, Response
+import requests
 from flask import abort, jsonify
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
+from google.cloud import storage
 
 
 def function_discord_bot(request: Request):
@@ -17,24 +19,22 @@ def function_discord_bot(request: Request):
     body = request.data.decode("utf-8")
 
     try:
-        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+        verify_key.verify(f'{timestamp}{body}'.encode(),
+                          bytes.fromhex(signature))
     except BadSignatureError:
         abort(401, 'invalid request signature')
-
 
     # Handle valid requests
     if request.json["type"] == 1:
         return reply_to_ping()
     else:
-        attachments = validate_list(request)
-        print(f"DEBUG: attachments: {attachments}")
         return jsonify({
             "type": 4,
             "data": {
                 "tts": False,
-                "content": "check da logs",
+                "content": validate_list(request),
                 "embeds": [],
-                "allowed_mentions": { "parse": [] }
+                "allowed_mentions": {"parse": []}
             }
         })
 
@@ -42,14 +42,36 @@ def function_discord_bot(request: Request):
 def validate_list(request: Request):
     data = request.json["data"]
     message_id = data["target_id"]
+    attachment = data["resolved"]["messages"][message_id]["attachments"]
+    url = attachment["url"]
+    filename = attachment["filename"]
 
-    return data["resolved"]["messages"][message_id]["attachments"]
+    download_file_path = f"/tmp/{filename}"
+    upload_bucket = "tournament-lists"
+
+    r = requests.get(url, allow_redirects=True)
+    open(download_file_path, 'wb').write(r.content)
+
+    upload_blob(upload_bucket, download_file_path, filename)
+
+    return f"File: {filename} uploaded to bucket: {upload_bucket}"
 
 
 def reply_to_ping():
     return jsonify({"type": 1})
 
 
+def upload_blob(bucket_name, file_path, destination_blob_name) -> None:
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(file_path)
+
+    print('File {} uploaded to {}.'.format(
+        destination_blob_name,
+        bucket_name))
 
 if __name__ == "__main__":
     # Register bot slash commands
@@ -59,23 +81,22 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    
     TOKEN = os.getenv('DISCORD_TOKEN')
     APP_ID = os.getenv('DISCORD_APP_ID')
 
     url = f"https://discord.com/api/v8/applications/{APP_ID}/commands"
     headers = {
         "Authorization": f"Bot {TOKEN}",
-        "Accept":"application/json",
+        "Accept": "application/json",
         "User-Agent": "",
         "Content-Type": "application/json"
-    }   
+    }
 
     # This is an example CHAT_INPUT or Slash Command, with a type of 1
     json = {
-    "name": "validate",
-    "type": 3
-}
+        "name": "validate",
+        "type": 3
+    }
 
     r = requests.post(url, headers=headers, json=json)
 
