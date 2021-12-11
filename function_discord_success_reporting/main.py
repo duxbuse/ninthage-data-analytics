5872ab28ee9cac74e77a0a730f3c1b25bc5a9d00
Sean <duxbuse@gmail.com>
2021-12-12 01:52:30 +1100

    more smart ness way of doing proper 6k char limit

--- function_discord_success_reporting/main.py
+++ function_discord_success_reporting/main.py
@@ -85,10 +85,24 @@ def truncate_field(value: str) -> str:
 
 def truncate_message(message: dict):
     DISCORD_EMBED_TOTAL_LENGTH = 6000
-    while len(json.dumps(message)) > DISCORD_EMBED_TOTAL_LENGTH:
+    current_total = character_total(message)
+    while current_total > DISCORD_EMBED_TOTAL_LENGTH:
         # Drop the last non footer field until passing the discord requirements
-        message["embeds"][0]["fields"] = message["embeds"][0]["fields"][:-2] + message["embeds"][0]["fields"][-1]
+        message["embeds"][0]["fields"] = message["embeds"][0]["fields"][:-3] + message["embeds"][0]["fields"][-1]
+        # reduce the character count buy what was just removed
+        current_total -= len(json.dumps(message["embeds"][0]["fields"][-2]["name"]))
+        current_total -= len(json.dumps(message["embeds"][0]["fields"][-2]["value"]))
 
+def character_total(message: dict) -> int:
+    embed = message["embeds"][0]
+    total = 0
+    total += len(json.dumps(embed["title"]))
+    total += len(json.dumps(embed["description"]))
+    total += len(json.dumps(embed["author"]["name"]))
+    for field in embed["fields"]:
+        total += len(json.dumps(field["name"]))
+        total += len(json.dumps(field["value"]))
+    return total
 
 if __name__ == "__main__":
     json_message = {'data': {'body': ""}}
