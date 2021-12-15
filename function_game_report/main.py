ed61b8e794589c13ad9e8d1d262396a8f2edf25b
Sean <duxbuse@gmail.com>
2021-12-15 16:48:56 +1100

    woops

--- function_game_report/main.py
+++ function_game_report/main.py
@@ -3,7 +3,7 @@ from flask.wrappers import Request
 
 def function_game_report(request: Request):
     print(f"{request=}")
-    return "Yay!!!!!", 200
+    return "Report uploaded", 200
 
 
 if __name__ == "__main__":
