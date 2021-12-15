656b30e9ca47e0140c2d8f13f4c7c4f0ca442949
Sean <duxbuse@gmail.com>
2021-12-15 21:35:44 +1100

    passing form data to workflow

--- function_game_report/main.py
+++ function_game_report/main.py
@@ -1,9 +1,31 @@
 from flask.wrappers import Request
+from google.cloud import workflows_v1beta
+from google.cloud.workflows import executions_v1beta
+from google.cloud.workflows.executions_v1beta.types import executions
+import json
 
 
 def function_game_report(request: Request):
-    print(f"{request.form=}")
-    return f"{request.form=}", 200
+    form_data = request.form.to_dict()
+    print(f"{form_data=}")
+
+    project = "ninthage-data-analytics"
+    location = "us-central1"
+    workflow = "workflow_parse_lists"
+
+    # Set up API clients.
+    execution_client = executions_v1beta.ExecutionsClient()
+    workflows_client = workflows_v1beta.WorkflowsClient()
+
+    # Construct the fully qualified location path.
+    parent = workflows_client.workflow_path(project, location, workflow)
+    execution = executions.Execution(argument=json.dumps(form_data))
+
+    # Execute the workflow.
+    response = execution_client.create_execution(parent=parent, execution=execution)
+    print(f"Created execution: {response.name}")
+
+    return f"{response=}\n{form_data=}", 200
 
 
 if __name__ == "__main__":
