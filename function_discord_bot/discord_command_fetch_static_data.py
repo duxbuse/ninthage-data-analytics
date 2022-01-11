700dd0761f544112a1f85d2ac9ae6f4f37837f59
Sean <duxbuse@gmail.com>
2022-01-11 17:07:08 +1100

    trying to auth

--- function_discord_bot/discord_command_fetch_static_data.py
+++ function_discord_bot/discord_command_fetch_static_data.py
@@ -1,11 +1,26 @@
 from flask.wrappers import Request
-import requests
+import urllib
+import google.auth.transport.requests
+import google.oauth2.id_token
 
 def fetch_static_data(request: Request):
     url = "https://us-central1-ninthage-data-analytics.cloudfunctions.net/function_static_data"
-    r = requests.get(url, allow_redirects=True)
-    print(r.status_code, r.text)
-    if r.status_code == 200:
-        return "Static data loading has begun"
-    else:
-        return "Static data loading has failed"
+
+    req = urllib.request.Request(url)
+
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, url)
+
+    req.add_header("Authorization", f"Bearer {id_token}")
+    response = urllib.request.urlopen(req)
+
+    # return response.read()
+
+    print("worked fine")
+
+
+
+    # if r.status_code == 200:
+    #     return "Static data loading has begun"
+    # else:
+    #     return "Static data loading has failed"
