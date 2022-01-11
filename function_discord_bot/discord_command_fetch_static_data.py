636ee10e2c3b0cb8b764d1b6d0180f0a0d6d878e
Sean <duxbuse@gmail.com>
2022-01-11 15:46:16 +1100

    not bothering with delete first

--- function_discord_bot/discord_command_fetch_static_data.py
+++ function_discord_bot/discord_command_fetch_static_data.py
@@ -4,4 +4,8 @@ import requests
 def fetch_static_data(request: Request):
     url = "https://us-central1-ninthage-data-analytics.cloudfunctions.net/function_static_data"
     r = requests.get(url, allow_redirects=True)
-    return "Static data loading has begun"
+    print(r.status_code, r.text)
+    if r.status_code == 200:
+        return "Static data loading has begun"
+    else:
+        return "Static data loading has failed"
