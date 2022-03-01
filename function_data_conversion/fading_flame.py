c9bb522ddc8aded751c4eb15158c8516b2d18f27
Sean <duxbuse@gmail.com>
2022-03-01 21:02:30 +1100

    handeling multiple date formats

--- function_data_conversion/fading_flame.py
+++ function_data_conversion/fading_flame.py
@@ -62,7 +62,10 @@ def armies_from_fading_flame(data:dict) -> list[ArmyEntry]:
         if not game_result:
             raise ValueError("No result is a big issue")
 
-        game_date = datetime.strptime(game_result.get("recordedAt"), "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+        try:
+            game_date = datetime.strptime(game_result.get("recordedAt"), "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+        except ValueError:
+            game_date = datetime.strptime(game_result.get("recordedAt"), "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
 
         player1 = game_result.get("player1", {})
         player2 = game_result.get("player2", {})
