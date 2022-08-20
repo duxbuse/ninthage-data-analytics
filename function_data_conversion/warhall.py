f7d0c883c774c98f261ead1cc6a69020a244a794
Sean <duxbuse@gmail.com>
2022-08-20 23:54:00 +1000

    Ignoring points checking cause warhall is getting it really wrong

--- function_data_conversion/warhall.py
+++ function_data_conversion/warhall.py
@@ -139,17 +139,18 @@ def armies_from_warhall(data: dict) -> list[ArmyEntry]:
             i - 1
         ].points_killed()
 
+    # TODO: Warhall is really borking the calculation so we shall skip this check until they get it sorted
     # check that calculated secondary points == points difference
     calculated_difference:int = abs(
         list_of_armies[0].round_performance[0].secondary_points
         - list_of_armies[1].round_performance[0].secondary_points
     )
     reported_difference:int = abs(data_obj.PlayersData[0].PointDifference)
-    # TODO: Workaround untill Warhall calculates it correctly
 
-    if (abs(calculated_difference - reported_difference) > 10): #Ideally this should be 0
-        errors.append(ValueError(f"{reported_difference=} != {calculated_difference=}"))
-        # errors.append(ValueError(f"Calculated difference:{ab1} should equal recorded difference:{ab2}"))
+    if (calculated_difference != reported_difference):
+        msg = f"{reported_difference=} != {calculated_difference=}\nCalculated - Player0: {list_of_armies[0].round_performance[0].secondary_points} Player1: {list_of_armies[1].round_performance[0].secondary_points}"
+        # errors.append(ValueError(msg))
+        print(msg)
 
     if errors:
         raise Multi_Error(errors)
@@ -158,120 +159,22 @@ def armies_from_warhall(data: dict) -> list[ArmyEntry]:
 
 
 if __name__ == "__main__":
-    # orcs vps = 4528
-    # koe vps = 1900
-    example = {
-        "Deployment": "Counterthrust",
-        "Map": 5,
-        "Objective": "Capture the Flags",
-        "PlayersData": [
-            {
-                "PlayerName": "Player1",
-                "ArmyName": "Orcs and Goblins",
-                "List": [
-                    "DEAD 510 - Orc Warlord, Feral Orc, War Boar, Shield, Light Armour (Tuktek's Guard),  Hand Weapon (Omen of the Apocalypse), Potion of Swiftness",
-                    "DEAD 455 - Orc Shaman, General, Feral Orc, Wizard Master, Shamanism, Rod of Battle",
-                    "DEAD 370 - Goblin King, Forest Goblin and Poison Attacks, Huntsmen Spider, Shield (Dusk Forged), Paired Weapons, Heavy Armour (Basalt Infusion),  Hand Weapon (Hero's Heart)",
-                    "DEAD 310 - Orc Chief, Feral Orc, Battle Standard Bearer (Aether Icon, Aether Icon), Great Weapon, Light Armour (Essence of Mithril), Obsidian Rock",
-                    "DEAD 120 - Goblin Chief, Common Goblin, Crown of the Wizard King",
-                    "DEAD 700 - 40 Orcs, Feral Orc, Bow, Spear, Standard Bearer (Green Tide), Musician, Champion",
-                    "DEAD 426 - 48 Goblins, Forest Goblin, Shield, Throwing Weapons, Standard Bearer (Green Tide), Musician",
-                    "DEAD 387 - 6 Trolls, Bridge Troll",
-                    "DEAD 135 - Gnasher Wrecking Team, ",
-                    "DEAD 135 - Gnasher Wrecking Team, ",
-                    "DEAD 80 - Scrap Wagon, ",
-                    "DEAD 80 - Scrap Wagon, ",
-                    "DEAD 175 - Greenhide Catapult, Splatterer, Orc Overseer",
-                    "DEAD 90 - Skewerer, ",
-                    "DEAD 525 - Gargantula, Web Launcher",
-                ],
-                "Objective": "Draw",
-                "PointDifference": -2998,
-                "Result": 4,
-            },
-            {
-                "PlayerName": "Player2",
-                "ArmyName": "Kingdom of Equitaine",
-                "List": [
-                    " 635 - Equitan Lord, General, Fey Steed, Shield, Bastard Sword (Shield Breaker), Percival's Panoply, Black Knight's Tabard, Dragonfire Gem, Sainted, Valour",
-                    " 480 - Equitan Lord, Pegasus Charger, Shield, Lance (Divine Judgement), Basalt Infusion, Paladin, Honour",
-                    " 355 - Equitan Lord, Battle Standard Bearer (Legion Standard, Legion Standard), Great Weapon (Supernatural Dexterity), Essence of Mithril, Justice",
-                    "DEAD 240 - Damsel, Wizard Adept, Divination, Lightning Vambraces",
-                    " 395 - 45 Lowborn Levies, Shield, Champion, Musician, Standard Bearer",
-                    "DEAD 335 - 6 Feudal Knights, Champion, Musician, Standard Bearer (Relic Shroud)",
-                    "DEAD 220 - 8 Ordo Sergeants, Great Weapon",
-                    "DEAD 180 - 15 Lowborn Archers, Longbow and Expert Bowmen, Musician",
-                    " 550 - 8 Knights Penitent, Champion (Ordo Minister and Orison), Standard Bearer",
-                    "DEAD 345 - 6 Knights Resplendent, ",
-                    "DEAD 220 - 3 Sky Heralds, Paired Weapons, Champion",
-                    " 185 - Sacred Reliquary, ",
-                    "DEAD 360 - Fey Knight, Stream and Springs",
-                ],
-                "Objective": "Draw",
-                "PointDifference": 2998,
-                "Result": 16,
-            },
-        ],
-        "ReportTime": 1643765781,
-    }
+    from main import download_blob
+    from os import remove
+    import json
 
-    # DE = 2603
-    # SA = 2098
-    example2 = {
-        "Deployment": "Marching Columns",
-        "Objective": "Capture the Flags",
-        "Map": 3,
-        "ReportTime": 1658612062,
-        "PlayersData": [
-            {
-                "PlayerName": "Player1",
-                "ArmyName": "Dread Elves",
-                "Result": 13,
-                "PointDifference": -106,
-                "Objective": "Won",
-                "List": [
-                    "DEAD 365 - Temple Exarch, General, Divination, War Smith, Blades of Darag, Obsidian Rock",
-                    "HALF 325 - Temple Exarch, Alchemy, Battle Standard Bearer, Binding Scroll, Lightning Vambraces",
-                    " 406 - 29 Silexian Spears, Musician",
-                    "DEAD 270 - 15 Silexian Auxiliaries, Musician",
-                    "HALF 270 - 15 Silexian Auxiliaries, Musician",
-                    " 180 - 5 Shadow Riders, ",
-                    "DEAD 570 - 28 Judicators, Champion",
-                    "HALF 440 - 3 Gorgons, Halberd",
-                    " 421 - 8 Warlock Acolytes, Champion",
-                    "DEAD 290 - 3 Thunder Pack, ",
-                    "HALF 200 - Repeater Battery, ",
-                    " 200 - Repeater Battery, ",
-                    "DEAD 200 - Repeater Battery, ",
-                    "HALF 180 - 5 Black Cloaks, ",
-                    " 180 - 5 Black Cloaks, ",
-                ],
-            },
-            {
-                "PlayerName": "Player2",
-                "ArmyName": "Saurian Ancients",
-                "Result": 7,
-                "PointDifference": 106,
-                "Objective": "Lost",
-                "List": [
-                    "DEAD 365 - 20 Skink Braves, Shield, 2x Caiman, Standard Bearer (Banner of Speed), Musician, Champion",
-                    "HALF 300 - 15 Saurian Warriors, Standard Bearer (Flaming Standard), Musician, Champion",
-                    " 355 - 15 Temple Guard, Standard Bearer, Musician, Champion",
-                    "DEAD 304 - 4 Caimans, Halberd, Standard Bearer, Musician, Champion",
-                    "HALF 255 - 5 Raptor Riders, Standard Bearer, Musician, Champion",
-                    " 115 - 2 Snake Swarms, ",
-                    "DEAD 235 - 3 Rhamphodon Riders, Champion",
-                    "HALF 185 - 3 Pteradon Sentries, Poisoned Javelin, Champion",
-                    " 160 - Weapon Beasts, Salamander",
-                    "DEAD 140 - Weapon Beasts, Spearback",
-                    "HALF 140 - 5 Chameleons, Champion",
-                    " 115 - 5 Skink Hunters, Shield and Poisoned Javelin, Champion",
-                    "DEAD 470 - Taurosaur, Giant Blowpipes",
-                    "HALF 285 - Stygiosaur, ",
-                    " 240 - Thyroscutus, Altar of the Snake-God",
-                ],
-            },
-        ],
-    }
-    armies = armies_from_warhall(example)
-    print(bool(armies))
+    file_name = "f1875910-29a3-4252-93b6-231bfd620b13.json"
+    download_file_path = f"./{file_name}"
+
+    downloaded_warhall_blob = download_blob("warhall", file_name)
+    downloaded_warhall_blob.download_to_filename(download_file_path)
+    if downloaded_warhall_blob:
+        print(
+            f"Downloaded {file_name} from warhall to {download_file_path}"
+        )
+    with open(download_file_path, "r") as json_file:
+        data = json.load(json_file)
+        print(f"Loaded data")
+    remove(download_file_path)
+    list_of_armies = armies_from_warhall(data)
+    print(f"Finished")
