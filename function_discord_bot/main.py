d5a4e9ae050d834b532ff493d38e066524c18725
Sean <duxbuse@gmail.com>
2023-05-01 20:06:45 +1000

    adding more time between retries to avoid rate limiting

--- function_discord_bot/main.py
+++ function_discord_bot/main.py
@@ -5,7 +5,6 @@ from flask import jsonify
 from security_headers import check_security_headers
 from discord_command_upload import upload_file
 from discord_command_fetch_static_data import fetch_static_data
-from discord_command_list_events import list_events
 
 
 def function_discord_bot(request: Request):
@@ -34,7 +33,6 @@ def function_discord_bot(request: Request):
 registered_commands = {
     "upload": upload_file,
     "fetch-static-data" : fetch_static_data,
-    # "list_events": list_events
 }
 
 if __name__ == "__main__":
--- function_upload_data_into_bigquery/main.py
+++ function_upload_data_into_bigquery/main.py
@@ -125,6 +125,7 @@ def function_upload_data_into_bigquery(
                 location="us-central1",  # Must match the destination dataset location.
                 job_config=job_config,
                 num_retries=20, #sometimes we get rate limited
+                timeout=5.0,
             )  # API request
 
         try:
